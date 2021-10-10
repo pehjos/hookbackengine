c7bd36f954eed6f13483cf8203fce6e675d6c9f3
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-10 05:52:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
  dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-// app.use(cors());
+ app.use(cors());
 
 
 // uninitialize git repo
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 res.send('heello hook back is working')
 
 })
-// const CONNECTION_URL = 'mongodb+srv://hookwall__123:hook123@cluster0.odwoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
