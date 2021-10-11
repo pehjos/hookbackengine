a3d81bae7e3ea74d7ee0115d1b06b64ef1d62658
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-11 10:46:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
  app.use(cors());
 
 app.use((req,res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin',"https://eloquent-lichterman-4bcfd1.netlify.app");
+  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000/");
   res.setHeader('Access-Control-Allow-Headers',"*");
   res.header('Access-Control-Allow-Credentials', true);
   next();
