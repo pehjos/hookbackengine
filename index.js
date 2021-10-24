13efe885eacdb15bc2945eb7295d3ad6f4faee89
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-24 08:59:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,15 @@ const app = express();
  dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors({
-    origin: '*'
-}));
-
+var allowlist = ['http://localhost:3000/', 'http://localhost:3000/']
+var corsOptionsDelegate = function (req, callback) { 
+ var corsOptions; if (allowlist.indexOf(req.header('Origin')) !== -1) 
+ { corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response }
+  else { corsOptions = { origin: false } // disable CORS for this request } callback(null, corsOptions)
+        // callback expects two parameters: error and options } 
+       app.get('/', cors(corsOptionsDelegate),
+        function (req, res, next) { res.json({msg: 'heello hook back is working'}) }) 
+    
 // app.use((req,res, next)=>{
 //   res.setHeader('Access-Control-Allow-Origin',"*");
 //   res.setHeader('Access-Control-Allow-Headers',"*");
@@ -24,10 +29,10 @@ app.use(cors({
 // rmdir /s .git
 app.use('/posts', postRoutes);
 app.use("/user", userRoutes);
-app.get('/' ,(req, res) => {
-res.send('heello hook back is working')
+// app.get('/' ,(req, res) => {
+// res.send('heello hook back is working')
   
-})
+// })
 
 const PORT = process.env.PORT|| 5000;
 
