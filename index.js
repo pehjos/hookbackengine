8972f19224f32db2dd20d750c5259ba1ee03cdd1
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-23 20:38:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
  dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
- app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 // app.use((req,res, next)=>{
 //   res.setHeader('Access-Control-Allow-Origin',"*");
