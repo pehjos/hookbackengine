14bdaf434e1d3be6f6bd8b4191fadb6be89eb4e0
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-25 10:26:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const app = express();
 
  dotenv.config()
 
-app.options('*', cors()); // enable pre-flight app.use(bodyParser.json());
+app.use(cors())
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 app.use((req,res, next)=>{
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000/");
   res.setHeader('Access-Control-Allow-Headers',"*");
   res.header('Access-Control-Allow-Credentials', true);
   next();
