6b5c3e94728f322e73fec01a9a837b58a609b0c2
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-25 10:05:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 app.use((req,res, next)=>{
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
   // res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
