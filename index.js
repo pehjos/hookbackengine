b5548d290dee53633c92e429315052cd14c84776
pehjos <71464455+pehjos@users.noreply.github.com>
2021-10-25 10:38:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use((req,res, next)=>{
   res.header("Access-Control-Allow-Origin", "https://api.cloudinary.com/v1_1/pehjos-inc/video/upload");
    res.header("Access-Control-Allow-Origin", "https://api.cloudinary.com/v1_1/pehjos-inc/image/upload");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
   res.setHeader('Access-Control-Allow-Origin',"*");
